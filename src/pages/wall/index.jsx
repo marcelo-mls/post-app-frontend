7e88aa3e42e39c437f115dc8ef692fb76b8a87ba
Marcelo Marques <marcelo-mls@hotmail.com>
2023-03-17 01:25:45 -0300

    feat(wall): add post form
    creates the mechanism to make a post

--- src/pages/wall/index.jsx
+++ src/pages/wall/index.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
 import postsMock from '../../../mock';
 
 export default function Wall() {
   const [posts, setPosts] = useState([]);
+  const [newPost, setNewPost] = useState('');
+  const [disableButton, setDisableButton] = useState(true);
 
   const fetchPosts = () => {
     const updatedPosts = postsMock.map((post) => ({
@@ -13,12 +18,45 @@ export default function Wall() {
     setPosts(updatedPosts);
   };
 
+  const handleAddPost = () => {
+    const userPost = {
+      id: 0,
+      initials: 'GU',
+      user: 'Guest User',
+      post: newPost,
+    };
+    setPosts([userPost, ...posts]);
+  };
+
+  const handleDisableButton = () => newPost.length > 3;
+
   useEffect(() => {
     fetchPosts();
   }, []);
 
+  useEffect(() => {
+    setDisableButton(!handleDisableButton());
+  }, [newPost]);
+
   return (
     <>
+      <div>
+        <TextField
+          id="filled-basic"
+          label="Type here..."
+          variant="filled"
+          value={newPost}
+          onChange={(e) => setNewPost(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          endIcon={<AddIcon />}
+          onClick={handleAddPost}
+          disabled={disableButton}
+        >
+          Post
+        </Button>
+      </div>
       {posts.map((post) => (
 
         <div key={post.id}>
